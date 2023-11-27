8fd0cb4567988efe8a8fc74dbdfadf8ed7326763
ThomasCodeKing <142684634+ThomasCodeKing@users.noreply.github.com>
2023-11-27 01:19:35 -0500

    Add files via upload

---
+++ player.py
@@ -0,0 +1,44 @@
+import pygame
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self,pos, constraint, speed):
+        super().__init__()
+        self.image = pygame.image.load("images\\player.png").convert_alpha() #64x64
+        self.rect = self.image.get_rect(midbottom = pos)
+        self.speed = speed
+        self.max_x_constraint = constraint
+        self.ready = True
+        self.laser_time = 0
+        self.laser_cooldown = 600
+
+    def get_input(self):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
+            self.rect.x += self.speed
+        elif keys[pygame.K_LEFT] or keys[pygame.K_a]:
+            self.rect.x -= self.speed
+
+        if keys[pygame.K_Space] and self.ready:
+            self.shoot_laser()
+            self.ready = False
+            self.laser_time = pygame.time.get_ticks()
+
+    def recharge(self):
+        if not self.ready:
+            current_time = pygame.time.get_ticks()
+            if current_time - self.laser_time >= self.laser_cooldown:
+                self.ready = True
+    def constraint(self):
+        if self.rect.left <= 0:
+            self.rect.left = 0
+        if self.rect.right >= self.max_x_constraint:
+            self.rect.right = self.max_x_constraint
+
+    def shoot_laser(self):
+        print("Pew!")
+
+    def update(self):
+        self.get_input()
+        self.constraint()
+        self.recharge()
